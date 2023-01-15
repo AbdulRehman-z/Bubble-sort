a7a2642b5db56070746773559e39e8db16a9845e
Abdul Rehman <yousafbhaikhan10@gmail.com>
2023-01-15 20:45:27 +0500

    implemented linkedList

---
+++ src/LinkedList.ts
@@ -0,0 +1,76 @@
+export {};
+class Node {
+  next: Node | null = null;
+
+  constructor(public data: number) {}
+}
+
+export class LinkedList {
+  head: Node | null = null;
+
+  add(data: number): void {
+    const node = new Node(data);
+
+    if (!this.head) {
+      this.head = node;
+      return;
+    }
+
+    let tail = this.head;
+
+    while (tail.next) {
+      tail = tail.next;
+    }
+
+    tail.next = node;
+  }
+
+  get length(): number {
+    if (!this.head) {
+      return 0;
+    }
+
+    let length = 1;
+    let node = this.head;
+    while (node.next) {
+      length++;
+      node = node.next;
+    }
+
+    return length;
+  }
+
+  at(index: number): Node {
+    if (!this.head) throw new Error("List is empty");
+
+    let counter = 0;
+    let node: Node | null = this.head;
+    while (node) {
+      if (counter === index) {
+        return node;
+      }
+
+      counter++;
+      node = node.next;
+    }
+
+    throw new Error("Out of reach index");
+  }
+
+  compare(leftIndex: number, rightIndex: number): Boolean {
+    if (!this.head) {
+      throw new Error("CANT COMPARE");
+    }
+
+    return this.at(leftIndex).data > this.at(rightIndex).data;
+  }
+
+  swap(leftIndex: number, rightIndex: number): void {
+    const leftNode = this.at(leftIndex).data;
+    const rightNode = this.at(rightIndex).data;
+
+    const leftSide = leftNode;
+    this.at(leftIndex).data = rightNode;
+    this.at(rightIndex).data = leftSide;
+  }
+}
