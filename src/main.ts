1808211abaff07afeba295a3a38910b153a51a1c
Abdul Rehman <yousafbhaikhan10@gmail.com>
2023-01-15 18:46:09 +0500

    implemented sorting class for strings

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 export {};
+import { CharacterCollection } from "./CharactersCollection";
 import { NumberCollection } from "./NumberCollection";
 import { Sorter } from "./Sorter";
 
 const numberCollection = new NumberCollection([10, 20, 80, 2, 3, -4]);
 
-const sorter = new Sorter(numberCollection);
+const characterCollection = new CharacterCollection(
+  "zyxwvutsrqponmlkjihgfedcba"
+);
+
+const sorter = new Sorter(characterCollection);
 
 sorter.sort();
